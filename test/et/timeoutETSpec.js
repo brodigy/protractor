f35699d060e69e2a6b4bc3759d92ba2335f32a21
petrealexandru <petrealexandru88@gmail.com>
2016-03-14 23:41:09 +0200

    added timeout test

---
+++ test/et/timeoutETSpec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+fdescribe('timeout page', function () {
+    var LoginPage = require('./../pages/login.po.js');
+    var TimeoutPage = require('./../pages/timeout.po.js');
+    var loginPage, timeoutPage, params;
+
+    beforeEach(function () {
+        loginPage = new LoginPage();
+        timeoutPage = new TimeoutPage();
+
+        loginPage.get();
+        params = browser.params.login;
+    });
+
+    it('should successfully redirect to login page after visiting the redirect page', function () {
+        expect(browser.getLocationAbsUrl()).toMatch('/login');
+        loginPage.setCredentials(params.email, params.password);
+        expect(loginPage.submitLogin.isEnabled()).toBeTruthy();
+        loginPage.submitLogin.click();
+
+        expect(browser.getLocationAbsUrl()).toMatch('/home');
+
+        browser.get('/#/timeout');
+        expect(timeoutPage.timeoutText.isPresent()).toBeTruthy();
+
+        browser.wait(protractor.ExpectedConditions.presenceOf(loginPage.submitLogin), 6000, 'Was redirected to login page');
+
+        expect(loginPage.submitLogin.isPresent()).toBeTruthy();
+    });
+
+});
---
+++ test/pages/timeout.po.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var TimeoutPage = function () {
+
+    this.timeoutText = element(by.id('timeout'));
+};
+
+module.exports = TimeoutPage;
