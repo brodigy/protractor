5dbd43acc6e444a857f21aab41b6b288dc51d655
petrealexandru <petrealexandru88@gmail.com>
2016-03-20 19:36:19 +0200

    added user form and service

--- app/js/controllers/GridController.js
+++ app/js/controllers/GridController.js
@@ -1,5 +1,38 @@
-app.controller('GridController', ['$scope',
-    function ($scope)  {
+app.controller('GridController', ['$scope', 'AuthenticationService', 'growl',
+    function ($scope, AuthenticationService, growl)  {
+
+        $scope.user = {
+            username : '',
+            firstName : '',
+            lastName : '',
+            gender : 'Select Gender',
+            password : ''
+        };
+
+        $scope.selectGender = function(val){
+            $scope.user.gender =  val;
+        };
+
+        $scope.isFormInvalid = function () {
+            return ($scope.userForm.username.$dirty && !$scope.userForm.username.$valid)
+                || ($scope.userForm.password.$dirty && !$scope.userForm.password.$valid);
+        };
+
+        $scope.isReadyToSave = function () {
+            return $scope.userForm.$valid;
+        };
+
+        $scope.saveUser = function(){
+            console.log(angular.toJson($scope.user));
+            AuthenticationService.saveUser($scope.user).then(
+                function(data) {
+                    $state.go('grid');
+                },
+                function(error) {
+                    growl.addErrorMessage('Something went wrong when saving your user');
+                }
+            );
+        };
 
         $scope.theGrid = {
             paginationPageSizes: [5, 10, 25],
--- app/js/service/AuthenticationService.js
+++ app/js/service/AuthenticationService.js
@@ -1,13 +1,31 @@
 app.factory('AuthenticationService', ['$q', '$http', function($q, $http) {
 
+    var BASE_URL= 'http://localhost:9090';
+
 	return {
 		login : function(credentials) {
-			return $http.get('http://localhost:9090/login', {
+			return $http.get(BASE_URL + '/login', {
 					params: {
 						email: credentials.email,
 						password: credentials.password
 					}
 			});
+		},
+		saveUser: function (user) {
+			var deferred = $q.defer();
+
+			$http.post(BASE_URL + '/saveUser', user)
+				.success(function (data) {
+					var result = {};
+					if (data) {
+						result = angular.fromJson(data);
+					}
+					deferred.resolve(result);
+				})
+				.error(function (data) {
+					deferred.reject(angular.fromJson(data));
+				});
+			return deferred.promise;
 		}
 	};
 
